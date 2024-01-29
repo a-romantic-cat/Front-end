bb10963e070bd327d004760b67aa41c0476efafd
gitaehee <rainwifekth@naver.com>
2024-01-29 18:19:54 +0900

    docs: 홈화면1에 navigate 추가

--- front-end/src/components/Home/Mainpage.jsx
+++ front-end/src/components/Home/Mainpage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from "react-router-dom";
 import Header from '../Header/Header';
 
 
@@ -31,14 +32,124 @@ const Container4 = styled.div`
   height: 1080px;
 `;
 
+const LinkContainer = styled.div`
+  width: 78px;
+  height: 151px;
+  margin: 276px 0 0 369px;
+  display: inline-flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+//내 우편함
+const MyLetter = styled.div`
+  color: #000;
+  font-family: "Gowun Dodum";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.2px;
+  cursor: pointer;
+`;
+
+//주소록
+const Address = styled.div`
+  color: #000;
+  font-family: "Gowun Dodum";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.2px;
+  cursor: pointer;
+`;
+
+//낭만 우편함
+const Romantic = styled.div`
+  color: #000;
+  font-family: "Gowun Dodum";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.2px;
+  cursor: pointer;
+`;
+
+//상점
+const Store = styled.div`
+  width: 78px; // 상점하고 내정보 줄바꿈 되게
+  color: #000;
+  font-family: "Gowun Dodum";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.2px;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+
+//내 정보
+const MyInfo = styled.div`
+  color: #000;
+  font-family: "Gowun Dodum";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.2px;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+
 export default function Mainpage() {
+
+  const navigate = useNavigate();
+
+  const navigateToMyBoxMain = () => {
+    navigate("/MyLetterbox");
+  };
+
+  const navigateToAddress = () => {
+    navigate("/AddressBook");
+  };
+
+  const navigateToRomantic = () => {
+    navigate("/RomanticLetterbox");
+  };
+
+  const navigateToStore = () => {
+    navigate("/Store");
+  };
+
+  const navigateToMyInfo = () => {
+    navigate("/MyPage");
+  };
   
     return (
       <div>
         <Header />
 
         <Container1>
-
+          <LinkContainer>
+            <MyLetter onClick={navigateToMyBoxMain}>
+              내 우편함
+            </MyLetter>
+            <Address onClick={navigateToAddress}>
+              주소록
+            </Address>
+            <Romantic onClick={navigateToRomantic}>
+              낭만 우편함
+            </Romantic>
+            <Store onClick={navigateToStore}>
+              상점
+            </Store>
+            <MyInfo onClick={navigateToMyInfo}>
+              내 정보
+            </MyInfo>
+          </LinkContainer>
         </Container1>
 
         <Container2>
