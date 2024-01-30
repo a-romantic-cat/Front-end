f8424b41de5b59c5d52bbbb4ebdcd97f997509e2
안혜연 <ahy020606@naver.com>
2024-01-31 03:47:56 +0900

    feat: 느린 우편함 달력 생성

--- front-end/src/components/MyLetterbox/SlowLetterboxToday.jsx
+++ front-end/src/components/MyLetterbox/SlowLetterboxToday.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Header from '../Header/Header';
 import '../../index.css';
-import DatePicker from "react-datepicker"; //달력 기능 위해 react-datepicker 라이브러리 설치
-import "react-datepicker/dist/react-datepicker.css";
 import Slow1 from '../../assets/img/Slow1.svg';
 import Slow2 from '../../assets/img/Slow2.svg';
 import Footprint from '../../assets/img/발자국.svg';
-import LeftTitle from '../../assets/img/달력제목왼쪽.svg';
-import RightTitle from '../../assets/img/달력제목오른쪽.svg';
 import MaskingTape from '../../assets/img/MaskingTape.svg';
+import DatePicker, { registerLocale } from 'react-datepicker'; //달력 기능 위해 react-datepicker 라이브러리 설치
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from 'date-fns/locale'; // 한국어 변경 위해 설치
+import { createGlobalStyle } from 'styled-components';
+import { format } from 'date-fns';
 
 const SlowLetterboxContainer = styled.div`
   width: 60px;
@@ -86,138 +87,6 @@ const FootprintImg = styled.img`
   position: absolute;
   left: 1267px;
   top: 474.97px;
-`;
-
-//달력
-const StyledContainer = styled.div`
-  width: 375px;
-  height: 389px;
-  position: absolute;
-  left: 465px;
-  top: 1080px;
-`;
-
-const StyledTitle = styled.div`
-  width: 133px;
-  height: 18px;
-  left: 119px;
-  top: 0;
-  position: absolute;
-`;
-
-const LeftTitleImg = styled.img`
-  width: 8px;
-  height: 14px; 
-  position: absolute;
-  left: 0px;
-  top: 2px;
-`;
-
-const RightTitleImg = styled.img`
-  width: 8px;
-  height: 14px; 
-  position: absolute;
-  right: 0px;
-  top: 2px;
-`;
-
-const StyledTitleText = styled.div`
-  left: 24px;
-  top: 0;
-  position: absolute;
-  color: black;
-  font-size: 18px;
-  font-family: 'Gowun Dodum';
-  font-weight: 400;
-  line-height: 18px;
-  word-wrap: break-word;
-`;
-
-const StyledContent = styled.div`
-  width: 368px;
-  height: 298px;
-  left: 4px;
-  top: 91px;
-  position: absolute;
-  display: inline-flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-end;
-  gap: 12px;
-`;
-
-const StyledRow = styled.div`
-  display: inline-flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  gap: 3px;
-`;
-
-const StyledItem = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  gap: 8px;
-`;
-
-const StyledCircle = styled.div`
-  width: 50px;
-  height: 50px;
-  border-radius: 9999px;
-  position: relative;
-`;
-
-const StyledText = styled.div`
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  top: 15px;
-  left: 15px;
-  text-align: center;
-  color: ${props => props.isGray ? '#9F9F9F' : '#212121'};
-  font-size: 18px;
-  font-family: 'Gowun Dodum';
-  font-weight: 400;
-  line-height: 18px;
-  word-wrap: break-word;
-`;
-
-const StyledForLastLine = styled.div`
-  flex-direction: column;
-  justify-content: flex-start
-  align-items: flex-start;
-  gap: 12px;
-  display: flex;
-`;
-
-const StyledBorder = styled.div`
-  width: 369px;
-  height: 0;
-  left: 4px;
-  top: 73px;
-  position: absolute;
-  border: 1px #9F9F9F solid;
-`;
-
-const StyledWeekdays = styled.div`
-  width: 335px;
-  height: 26px;
-  padding: 12px 20px;
-  left: 0;
-  top: 26px;
-  position: absolute;
-  display: inline-flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  gap: 36px;
-`;
-
-const Weekday = styled.div`
-  color: black;
-  font-size: 18px;
-  font-family: 'Gowun Dodum';
-  font-weight: 400;
-  word-wrap: break-word;
 `;
 
 function TextLines({ text }) {
@@ -355,6 +224,149 @@ const BlurBackground = styled.div`
   position: absolute;
 `;
 
+//달력 관련 스타일
+const GlobalStyles = createGlobalStyle`
+  & .react-datepicker__current-month,
+  & .react-datepicker-time__header,
+  & .react-datepicker__time-pickr,
+  & .react-datepicker__day,
+  & .react-datepicker__time-name {
+    font-family: 'Gowun Dodum' !important;
+  }
+
+  .react-datepicker {
+    border: none !important;
+    box-shadow: none !important;
+    background-color: transparent !important;
+  }
+
+  .react-datepicker__header {
+    background-color: transparent !important;
+    width: 375px;
+    height: 76px;
+    border: none;
+  }
+
+  .react-datepicker__navigation-icon::before {
+    position: absolute;
+    top: 2px;
+    border-color: black;
+    border-width: 2px 2px 0 0;
+  }
+
+  .react-datepicker__current-month {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 18px;
+    color: black;
+    word-wrap: break-word;
+    margin-bottom: 8px;
+  }
+
+  .react-datepicker__day-names {
+    width: 335px; 
+    height: 26px; 
+    padding: 12px 20px;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 36px;
+    display: inline-flex;
+    margin: 0;
+    border-bottom: 1px #9F9F9F solid;
+  }
+
+  .react-datepicker__day-name {
+    font-family: 'Gowun Dodum' !important;
+    color: #212121;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 18px;
+    word-wrap: break-word;
+    width: 16px;
+    height: 26px;
+    margin: 0;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+
+  .react-datepicker__month {
+    width: 368px;
+    height: 298px;
+    margin: 15px 0 0 0;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-end;
+    gap: 12px;
+    display: inline-flex;
+    position: absolute;
+    left: 4px;
+    top: 91px;
+  }
+
+  .react-datepicker__week {
+    width: 368px;
+    height: 50px;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 3px;
+    display: inline-flex;
+  }
+
+  .react-datepicker__day {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #212121;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 18px;
+    word-wrap: break-word;
+    width: 50px;
+    height: 50px;
+  }
+
+  .react-datepicker__day--selected {
+    background-color: rgba(243, 231, 231, 0.70) !important;
+    border-radius: 9999px;
+  }
+
+  .react-datepicker__day:hover {
+    background-color: transparent !important;
+  }
+`;
+
+// 기존의 한국어 로케일 설정을 복사해서 새로운 로케일 설정을 생성
+const customKoLocale = {
+  ...ko,
+  formatLong: {
+    ...ko.formatLong,
+    date: ({ date, width }) => {
+      const formatDate = width === 'abbreviated' ? 'MM월' : 'yyyy년 MM월';
+      return format(date, formatDate);
+    },
+  },
+};
+
+registerLocale({ko}, customKoLocale);
+
+//react-datepicker
+const DatepickerComponent = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div>
+      <GlobalStyles />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          inline
+          locale={ko}
+        />
+    </div>
+  );
+};
+
 export default function SlowLetterboxToday() {
   const text = "오늘 하루 행복했나요?\n\n길을 걷다 내 취향인 카페를 발견한 것, \n눈물 날 만큼 재밌는 영화를 본 것, \n사랑하는 사람과 시간을 보낸 것,\n.\n.\n.\n당신의 행복한 순간을 기록해 주세요.\n낭만고양이가\n매일의 행복을 모아 올해의 마지막 주에 전달해 드릴게요.\n소소한 행복들이 쌓여 큰 행복으로 돌아올 거예요."
   const placeholderText = "오늘 하루 행복했던 순간을 적어보세요.";
@@ -386,6 +398,7 @@ export default function SlowLetterboxToday() {
   return (
     <div>
       <Header />
+      <DatepickerComponent />
 
       <SlowLetterboxContainer>
         <Slow1Img src={Slow1} alt='Slow1' />
@@ -400,201 +413,6 @@ export default function SlowLetterboxToday() {
       <TextLines text={text} />
 
       <FootprintImg src={Footprint} alt='발자국' />
-
-      <StyledContainer>
-
-        <StyledTitle>
-          <LeftTitleImg src={LeftTitle} alt='왼쪽제목' />
-          <StyledTitleText>2024년 1월</StyledTitleText>
-          <RightTitleImg src={RightTitle} alt='오른쪽제목' />
-        </StyledTitle>
-
-        <StyledContent>
-          <StyledRow>
-            <StyledItem>
-              <StyledCircle>
-                <StyledText>1</StyledText>
-              </StyledCircle>
-            </StyledItem>
-            <StyledItem>
-              <StyledCircle>
-                <StyledText>2</StyledText>
-              </StyledCircle>
-            </StyledItem>
-            <StyledItem>
-              <StyledCircle>
-                <StyledText>3</StyledText>
-              </StyledCircle>
-            </StyledItem>
-            <StyledItem>
-              <StyledCircle>
-                <StyledText>4</StyledText>
-              </StyledCircle>
-            </StyledItem>
-            <StyledItem>
-              <StyledCircle>
-                <StyledText>5</StyledText>
-              </StyledCircle>
-            </StyledItem>
-            <StyledItem>
-              <StyledCircle>
-                <StyledText isGray={true}>6</StyledText>
-              </StyledCircle>
-            </StyledItem>
-          </StyledRow>
-
-          <StyledForLastLine>
-            <StyledRow>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>7</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>8</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>9</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>10</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>11</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>12</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>13</StyledText>
-                </StyledCircle>
-              </StyledItem>
-            </StyledRow>
-
-            <StyledRow>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>14</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>15</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>16</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>17</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>18</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>19</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>20</StyledText>
-                </StyledCircle>
-              </StyledItem>
-            </StyledRow>
-
-            <StyledRow>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>21</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle style={{background: 'rgba(243, 231, 231, 0.70)'}}>
-                  <StyledText>22</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>23</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>24</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>25</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>26</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>27</StyledText>
-                </StyledCircle>
-              </StyledItem>
-            </StyledRow>
-
-            <StyledRow>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText isGray={true}>28</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>29</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>30</StyledText>
-                </StyledCircle>
-              </StyledItem>
-              <StyledItem>
-                <StyledCircle>
-                  <StyledText>31</StyledText>
-                </StyledCircle>
-              </StyledItem>
-            </StyledRow>
-          </StyledForLastLine>
-        </StyledContent>
-
-        <StyledBorder />
-
-        <StyledWeekdays>
-          <Weekday>일</Weekday>
-          <Weekday>월</Weekday>
-          <Weekday>화</Weekday>
-          <Weekday>수</Weekday>
-          <Weekday>목</Weekday>
-          <Weekday>금</Weekday>
-          <Weekday>토</Weekday>
-        </StyledWeekdays>
-      </StyledContainer>
 
       {isOldWrapperVisible && (
         <Wrapper>
