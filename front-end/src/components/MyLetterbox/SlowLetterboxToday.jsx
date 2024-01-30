e1a5283988c8b6f6cede7b2a3bedb7447863cd50
안혜연 <ahy020606@naver.com>
2024-01-31 00:49:52 +0900

    feat: 느린 우편함 등록하기까지 생성

--- front-end/src/components/MyLetterbox/SlowLetterboxToday.jsx
+++ front-end/src/components/MyLetterbox/SlowLetterboxToday.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Header from '../Header/Header';
 import '../../index.css';
@@ -7,6 +7,7 @@ import Slow2 from '../../assets/img/Slow2.svg';
 import Footprint from '../../assets/img/발자국.svg';
 import LeftTitle from '../../assets/img/달력제목왼쪽.svg';
 import RightTitle from '../../assets/img/달력제목오른쪽.svg';
+import MaskingTape from '../../assets/img/MaskingTape.svg';
 
 const SlowLetterboxContainer = styled.div`
   width: 60px;
@@ -229,8 +230,141 @@ function TextLines({ text }) {
   );
 }
 
+//달력 오른쪽 작성란
+const Wrapper = styled.div`
+  width: 389px;
+  height: 487px;
+  position: relative;
+  left: 1066px;
+  top: 980px;
+`;
+
+const RedBox = styled.div`
+  width: 389px;
+  height: 42px;
+  left: 0;
+  top: 421px;
+  position: absolute;
+  background: #C90000;
+  border-radius: 10px;
+  cursor: pointer;
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+`;
+
+const RedBoxText = styled.div`
+  width: 63px;
+  height: 21px;
+  position: absolute;
+  margin: 10.5px 163px;
+  padding: 12.4px;
+  color: white;
+  font-size: 18px;
+  font-family: 'Pretendard';
+  font-weight: 500;
+  word-wrap: break-word;
+`;
+
+const Description = styled.div`
+  width: 163px;
+  height: 17px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  position: absolute;
+  color: #757575;
+  font-size: 14px;
+  font-family: 'Pretendard';
+  font-weight: 400;
+  word-wrap: break-word;
+`;
+
+const GrayBox = styled.div`
+  width: 310px;
+  height: 121px;
+  padding-bottom: 268px;
+  padding-left: 40px;
+  padding-right: 39px;
+  left: 0;
+  top: 0;
+  position: absolute;
+  background: #E7E7E7;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 25px;
+`;
+
+const MaskingTapeImg = styled.img`
+  width: 177px;
+  height: 53px;
+  position: absolute;
+  top: -27px;
+`;
+
+const TextBoxWrapper = styled.div`
+  width: 310px;
+  height: 70px;
+  position: relative;
+  left: 0;
+  top: 50px;
+`;
+
+const TextBox = styled.input`
+  width: 310px;
+  left: 0;
+  top: 43px;
+  position: absolute;
+  color: #757575;
+  font-size: 19px;
+  font-family: 'Gowun Dodum';
+  font-weight: 400;
+  line-height: 26.60px;
+  word-wrap: break-word;
+  border: none; 
+  outline: none;
+  background-color: transparent; /* 배경색 제거 */
+  cursor: pointer; /* 클릭 가능한 커서 스타일 */
+`;
+
+const DateText = styled.div`
+  left: 0;
+  top: 0;
+  position: absolute;
+  text-align: center;
+  color: black;
+  font-size: 20px;
+  font-family: 'Gowun Dodum';
+  font-weight: 400;
+  line-height: 20px;
+  word-wrap: break-word;
+`;
+
+
 export default function SlowLetterboxToday() {
   const text = "오늘 하루 행복했나요?\n\n길을 걷다 내 취향인 카페를 발견한 것, \n눈물 날 만큼 재밌는 영화를 본 것, \n사랑하는 사람과 시간을 보낸 것,\n.\n.\n.\n당신의 행복한 순간을 기록해 주세요.\n낭만고양이가\n매일의 행복을 모아 올해의 마지막 주에 전달해 드릴게요.\n소소한 행복들이 쌓여 큰 행복으로 돌아올 거예요."
+  const placeholderText = "오늘 하루 행복했던 순간을 적어보세요.";
+  const [isEditMode, setIsEditMode] = useState(false); // 편집 모드 상태를 관리하는 상태값
+  const [textBoxValue, setTextBoxValue] = useState(""); // TextBox의 값 상태값
+
+  const today = new Date(); // 오늘의 날짜를 가져옴
+  const year = today.getFullYear(); // 연도
+  const month = today.getMonth() + 1; // 월 (0부터 시작하므로 1을 더해줌)
+  const date = today.getDate(); // 날짜
+
+  const handleTextBoxClick = () => {
+    setIsEditMode(true); // 편집 모드 활성화
+  };
+
+  const handleRedBoxClick = () => {
+    setIsEditMode(false); // 편집 모드 종료
+  };
+
+  const handleTextBoxChange = (e) => {
+    setTextBoxValue(e.target.value); // TextBox의 값 변경
+  };
 
   return (
     <div>
@@ -444,6 +578,36 @@ export default function SlowLetterboxToday() {
           <Weekday>토</Weekday>
         </StyledWeekdays>
       </StyledContainer>
+
+      <Wrapper>
+        <RedBox onClick={handleRedBoxClick}>
+          <RedBoxText>등록하기</RedBoxText>
+        </RedBox>
+        <Description>등록 후에는 수정할 수 없어요.</Description>
+        <GrayBox>
+          <MaskingTapeImg src={MaskingTape} alt='MaskingTape' />
+          <TextBoxWrapper>
+            {isEditMode ? (
+              <TextBox
+                type="text"
+                placeholder={placeholderText}
+                value={textBoxValue}
+                onClick={handleTextBoxClick}
+                onChange={handleTextBoxChange}
+              />
+            ) : (
+              <TextBox
+                type="text"
+                placeholder={placeholderText}
+                value={textBoxValue || placeholderText}
+                onClick={handleTextBoxClick}
+                readOnly
+              />
+            )}
+            <DateText>{`${year}년 ${month}월 ${date}일`}</DateText>
+          </TextBoxWrapper>
+        </GrayBox>
+      </Wrapper>
     </div>
   )
 }
